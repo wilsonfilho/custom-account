c0ebe757a1348dc4f682073d6ae1512b3d5c90c2
wilson-pereira <wilson_pereira@apple.com>
2024-11-03 22:22:49 -0300

    Fix before commit

--- src/main/java/io/pismo/challenge/repository/AccountRepository.java
+++ src/main/java/io/pismo/challenge/repository/AccountRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByDocumentNumber(String documentNumber);
+
 }
--- src/main/java/io/pismo/challenge/repository/OperationTypeRepository.java
+++ src/main/java/io/pismo/challenge/repository/OperationTypeRepository.java
@@ -1,10 +1,7 @@
 package io.pismo.challenge.repository;
 
-import io.pismo.challenge.model.Account;
 import io.pismo.challenge.model.OperationType;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface OperationTypeRepository extends JpaRepository<OperationType, Long> {
 

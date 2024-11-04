c8358467c2b275f26c392c3389d0d05435e15599
wilson-pereira <wilson_pereira@apple.com>
2024-11-03 22:14:43 -0300

    Pismo Application

---
+++ src/main/java/io/pismo/challenge/PismoChallengeApplication.java
@@ -0,0 +1,13 @@
+package io.pismo.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PismoChallengeApplication.class, args);
+	}
+
+}

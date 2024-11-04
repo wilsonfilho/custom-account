604a5c71566d3f3e39d30f7793ac79cc670c8470
wilson-pereira <wilson_pereira@apple.com>
2024-11-04 00:33:07 -0300

    Remove unused import

--- src/main/java/io/pismo/challenge/repository/AccountRepository.java
+++ src/main/java/io/pismo/challenge/repository/AccountRepository.java
@@ -2,7 +2,6 @@ package io.pismo.challenge.repository;
 
 import io.pismo.challenge.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
